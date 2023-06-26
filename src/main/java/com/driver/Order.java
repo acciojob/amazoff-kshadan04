65d341152839064c2f2b6a7bb113b8eabdcc3a96
shadan khan <kshadan03@gmail.com>
2023-06-26 09:12:18 +0530

    AmazeOff

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,13 +9,24 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        String[] time = deliveryTime.split(":");
+        this.deliveryTime = Integer.parseInt(time[0])*60 + Integer.parseInt(time[1]);
     }
 
     public String getId() {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public int getDeliveryTime() {
         return deliveryTime;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
     }
 }
